ec1ae767532b3d921a19d3597938fb97d4d817a8
KaineBismarck <kaine.bismarck@suprbiz.io>
2024-03-07 04:33:27 +0100

    added individual routes

--- src/pages/dashboard/profile/Individual.tsx
+++ src/pages/dashboard/profile/Individual.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Individual() {
   return (
-    <div>Individual</div>
-  )
+    <div className="flex">
+      <div className="profile-card-shadow p-6 pt-10 flex flex-col justify-center bg-white">
+        <span className="">Jane Doe</span>
+      </div>
+    </div>
+  );
 }
