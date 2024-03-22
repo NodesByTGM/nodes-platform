49d2edb5c1c76462b38632c2212379963136dc9d
Elijah Soladoye <shodown96@gmail.com>
2024-03-22 08:59:08 +0100

    local server

--- src/utilities/config.ts
+++ src/utilities/config.ts
@@ -1,7 +1,7 @@
 export const BASE_API_ENDPOINT =
   process.env.NODE_ENV === "development"
-    ? "https://nodes-api.thegridmanagement.com/api/v1"
-    : "https://nodes-api.thegridmanagement.com/api/v1";
+    ? "http://localhost:3001/api/v1"
+    : "https://nodes-server-v1.onrender.com/api/v1";
 export const MAIN_APP_URL = "https://nodes.com";
 
 const API_ENDPOINTS = {
