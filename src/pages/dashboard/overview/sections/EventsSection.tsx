622ccce7371db205ca694c17e99475a320e396ab
KaineBismarck <kaine.bismarck@suprbiz.io>
2024-04-13 09:33:54 +0100

    added UI

--- src/pages/dashboard/overview/sections/EventsSection.tsx
+++ src/pages/dashboard/overview/sections/EventsSection.tsx
@@ -34,7 +34,7 @@ export default function EventsSection() {
           data={eventsData || []}
           event
           refetchEvents={allEventsRefetch}
-          title={`Trending Events`}
+          title={`Events`}
           description={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. `}
         />
       ) : null}
