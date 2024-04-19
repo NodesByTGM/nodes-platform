236d339b053ca816332be99942fcc9e869753d8e
KaineBismarck <kaine.bismarck@suprbiz.io>
2024-04-20 00:25:49 +0100

    landing page carousel done

--- src/components/landingPage/LandingPageCarousel.tsx
+++ src/components/landingPage/LandingPageCarousel.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
+import "../../assets/LandingPageCarousel.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
 
 export default function Carousel() {
+  const images = [
+    "/landingPageImg/LandingPageCarouselImg1.png",
+    "/landingPageImg/LandingPageCarouselImg2.png",
+    "/landingPageImg/LandingPageCarouselImg3.png",
+    "/landingPageImg/LandingPageCarouselImg4.png",
+    "/landingPageImg/LandingPageCarouselImg5.png",
+  ];
   return (
-    <div className='mt-20'>Carousel</div>
-  )
+    <div className="pt-20">
+      <Swiper
+        className="mySwiper landingPageSwiper"
+        slidesPerView={4.5}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        spaceBetween={5}
+        modules={[Autoplay]}
+      >
+        <div className="flex items-end">
+       
+          {images.map((image) => (
+            <div key={image} className="w-full">
+              <SwiperSlide>
+                <img src={image} alt="" className="" />
+              </SwiperSlide>
+            </div>
+          ))}
+        </div>
+      </Swiper>
+    </div>
+  );
 }
