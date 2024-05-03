2cf92da9078f3f1f38ad4dbcd633d015533ced83
KaineBismarck <kaine.bismarck@suprbiz.io>
2024-05-03 18:26:54 +0100

    ensuring username doesnt have spaces

--- src/utilities/validation.ts
+++ src/utilities/validation.ts
@@ -4,7 +4,7 @@ import { object, string, ref, bool, array, number } from "yup";
 export const signupSchema = object({
   name: string().required("Name is a required field"),
   email: string().email().required("Email is a required field"),
-  username: string().required("Username is a required field"),
+  username: string().required("Username is a required field").trim().matches(/^\S*$/, 'Username must not contain spaces'),
   dob: string().required("DOB field is required"),
   password: string().required("Password is a required field"),
   confirmPassword: string()
@@ -90,7 +90,7 @@ export type AdminSignupValidationType = {
 export const profileSchema = object({
   firstName: string().required("first name is a required field"),
   lastName: string().required("Last name is a required field"),
-  username: string().required("Username is a required field"),
+  username: string().required("Username is a required field").trim().matches(/^\S*$/, 'Username must not contain spaces'),
   avatar: object()
     .shape({
       id: string(),
@@ -451,7 +451,7 @@ export const PersonalIndividualInformationSchema = {
 
 export const accountSettingsSchema = object({
   name: string().required("Name is a required field"),
-  username: string().required("Username is a required field"),
+  username: string().required("Username is a required field").trim().matches(/^\S*$/, 'Username must not contain spaces'),
   email: string().email().required("Email is a required field"),
   day: string().required(),
   month: string().required(),
